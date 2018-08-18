e5d4a038afcc33fdc459fb841acea0b1f39ef8d8
CHOI <CHOI@DESKTOP-VCNR5OM>
2018-08-18 11:35:33 +0900

    imit

---
+++ src/space/co/work/ValueTest.java
@@ -0,0 +1,15 @@
+package space.co.work;
+
+public class ValueTest {
+
+	public static void main(String[] args) {
+		int aVal = 10;
+		int bVal = 5;
+		int Tot;
+		
+		Tot=aVal+bVal;
+		System.out.println(Tot);
+
+	}
+
+}
